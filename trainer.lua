98366fd22babd0a86aeba578dc80531d13fd96df
axetion <akt@stellarcore.net>
2017-06-03 08:31:28 -0700

    Cleaned up trainer, comments

--- trainer.lua
+++ trainer.lua
@@ -2,12 +2,12 @@ require "ability_data"
 require "nn"
 
 -- Hyper parameters
-local MINI_BATCH_SIZE = 100
-local PATIENCE = 15
-local EARLY_STOP_THRESHOLD = 0
+local MINI_BATCH_SIZE = 100 -- number of examples in a batch
+local PATIENCE = 15 -- how long we should put up with the validation error increasing before stopping
+local EARLY_STOP_THRESHOLD = 0 -- difference between the previous and current validation error (0 = any time the error increases)
 local HIDDEN_LAYERS = 3
 local LEARNING_RATE = .1
-local TRAINING_SET_SIZE = .8
+local TRAINING_SET_SIZE = .8 -- training/test data split (training 80%, test 20%)
 
 local function CreateContainer(input_layer, output_layer, hidden_layer)
 	local net = nn.Sequential()
@@ -35,11 +35,13 @@ local function Shuffle(data)
 	end
 end
 
+-- Label weights represents the weights of each criterion (so moving vs target vs items vs abilities), 
+-- class weights represents the weights of each class in the classification criterions (so specific items within the error for items)
 local function Loss(label_weights, class_weights)
 	local loss = nn.ParallelCriterion()
 
 	for i, label_weight in ipairs(label_weights) do
-		if i == 1 then
+		if i == 1 then -- first part is the move data
 			loss:add(nn.AbsCriterion(), label_weight)
 		else
 			local nll = nn.CrossEntropyCriterion(class_weights[i - 1])
@@ -53,14 +55,14 @@ local function Loss(label_weights, class_weights)
 end
 
 local function Train(net, data, loss, label_sizes)
-	Shuffle(data)
+	Shuffle(data) -- for randomizing the split
 
 	local validation_split = math.floor(#data * TRAINING_SET_SIZE)
 
-	local best = math.huge
+	local best = math.huge -- previous best validation error
 	local training_err = 0
 	local validation_err = 0
-	local patience_itr = 0
+	local patience_itr = 0 -- how long we've been waiting for validation error to go back down
 
 	print(net)
 
@@ -70,8 +72,9 @@ local function Train(net, data, loss, label_sizes)
 		training_err = 0
 		validation_err = 0
 
-		training = torch.randperm(validation_split)
+		training = torch.randperm(validation_split) -- shuffle training examples
 
+		-- train
 		for i = 1, training:size(1) do
 			local example = data[training[i]]
 
@@ -82,7 +85,7 @@ local function Train(net, data, loss, label_sizes)
 			local parts = {}
 			local start = 1
 
-			for index, size in ipairs(label_sizes) do
+			for index, size in ipairs(label_sizes) do -- split up the output into move data/classes...
 				parts[index] = output:sub(1, -1, start, start + size - 1)
 				start = start + size
 			end
@@ -93,6 +96,7 @@ local function Train(net, data, loss, label_sizes)
 			net:updateParameters(LEARNING_RATE)
 		end
 
+		-- calculate validation error
 		for i = validation_split + 1, #data do
 			local example = data[i]
 
@@ -111,14 +115,14 @@ local function Train(net, data, loss, label_sizes)
 
 		if best - validation_err < EARLY_STOP_THRESHOLD then
 			patience_itr = patience_itr + 1
-		else
+		else -- improvement was made
 			patience_itr = 0
 			best = validation_err
 		end	
 	end
 end
 
-local function ParseMoveBatch(examples, hero, team, totals, attacks)
+local function ParseMoveBatch(examples, hero, team, totals)
 	local batch_pos = 1
 	local input_batch = {}
 	local output_batch = {}
@@ -128,10 +132,10 @@ local function ParseMoveBatch(examples, hero, team, totals, attacks)
 	local num_active_items = #ability_data.activeItems[hero][team]
 	local num_active_abilities = #ability_data.activeAbilities[hero][team]
 
-	local input_view = 0
+	local input_view = 0 -- counter for size of the input feature vector, so that we can shape the input batch appropriately later
 
 	for example in examples do
-		if batch_pos > MINI_BATCH_SIZE then
+		if batch_pos > MINI_BATCH_SIZE then -- we've filled a batch, but there's still more examples left to read
 			more = true
 			break
 		end
@@ -164,25 +168,27 @@ local function ParseMoveBatch(examples, hero, team, totals, attacks)
 				if part == "output" then
 					state = 2
 				else
-					input[(tonumber(part) - 1) + items_pos] = 1.0
+					input[(tonumber(part) - 1) + items_pos] = 1.0 -- we have that item, set it to 1
 				end
 			elseif state == 2 then -- state 2: move location
-				if chunk_pos > 3 then
+				if chunk_pos > 3 then -- beginning of classes
 					state = 3
-					local label = tonumber(part)
+					local class = tonumber(part)
 
+					-- Increment count of occurences of this class
 					if totals[1] == nil then
 						totals[1] = {}
 					end
 
-					if totals[1][label] == nil then
-						for i = #totals[1] + 1, label - 1 do
+					if totals[1][class] == nil then
+						-- Make sure the array doesn't have any holes in it
+						for i = #totals[1] + 1, class - 1 do
 							totals[1][i] = 0
 						end
 
-						totals[1][label] = 1
+						totals[1][class] = 1
 					else	
-						totals[1][label] = totals[1][label] + 1
+						totals[1][class] = totals[1][class] + 1
 					end	
 
 					output[1] = torch.Tensor(move_info)
@@ -192,31 +198,27 @@ local function ParseMoveBatch(examples, hero, team, totals, attacks)
 				else
 					local move = tonumber(part)
 
-					if chunk_pos == 1 and move == 1 then
-						attacks = attacks + 1
-					end
-
 					move_info[chunk_pos] = move
 					chunk_pos = chunk_pos + 1
 				end
-			elseif state == 3 then
-				local label = tonumber(part)
+			elseif state == 3 then -- state 3: classification output
+				local class = tonumber(part)
 
 				if totals[chunk_pos - 1] == nil then
 					totals[chunk_pos - 1] = {}
 				end
 
-				if totals[chunk_pos - 1][label] == nil then
-					for i = #totals[chunk_pos - 1] + 1, label - 1 do
+				if totals[chunk_pos - 1][class] == nil then
+					for i = #totals[chunk_pos - 1] + 1, class - 1 do
 						totals[chunk_pos - 1][i] = 0
 					end
 
-					totals[chunk_pos - 1][label] = 1
+					totals[chunk_pos - 1][class] = 1
 				else	
-					totals[chunk_pos - 1][label] = totals[chunk_pos - 1][label] + 1
+					totals[chunk_pos - 1][class] = totals[chunk_pos - 1][class] + 1
 				end	
 
-				output[chunk_pos] = label
+				output[chunk_pos] = class
 
 				chunk_pos = chunk_pos + 1
 			end
@@ -236,32 +238,31 @@ local function ParseMoveBatch(examples, hero, team, totals, attacks)
 		batch_pos = batch_pos + 1
 	end
 
-	if batch_pos > 1 then
+	if batch_pos > 1 then -- did we actually get any examples
+		-- turn the batches into Tensors
 		output_batch[1] = torch.view(torch.cat(output_batch[1]), -1, batch_pos - 1)
 	
 		for i = 2, #output_batch do
 			output_batch[i] = torch.Tensor(output_batch[i])
 		end	
 
-		return {torch.view(torch.cat(input_batch), -1, input_view), output_batch}, more, attacks
+		return {torch.view(torch.cat(input_batch), -1, input_view), output_batch}, more
 	else
-		return nil, more, attacks
-	end		
+		return nil, more
+	end
 end
 
 local function LoadData(hero, team)
 	local path = string.format("data/%s/%d_", hero, team)
 
-	local move_data = {}
-	local move_pos = 0
-	local move_total = 0
+	local move_data = {} -- table of example batches
+	local move_pos = 0 -- current position in the table
+	local move_total = 0 -- total number of examples (not batches)
 
 	--local items_data = {}
 	--local items_pos = 0
 
 	local move_class_counts = {}
-
-	local attacks = 0
 
 	local move_lines = io.lines(path .. "moveexamples")
 	local more = true
@@ -269,7 +270,7 @@ local function LoadData(hero, team)
 	while more do
 		local batch
 		
-		batch, more, attacks = ParseMoveBatch(move_lines, hero, team, move_class_counts, attacks)
+		batch, more = ParseMoveBatch(move_lines, hero, team, move_class_counts)
 
 		if batch ~= nil then
 			move_data[move_pos] = batch
@@ -284,8 +285,9 @@ local function LoadData(hero, team)
 	--	item_pos = item_pos + 1
 	--end
 
-	local move_label_weights = {1} -- weights of each label
-	local move_class_weights = {} -- weights of each class in the labels
+	-- Calculate weights for the loss function
+	local move_label_weights = {1} -- weights of each label (move vs target vs abilities vs items...)
+	local move_class_weights = {} -- weights of each class in the labels (specific items or abilities within that)
 
 	for i, label in ipairs(move_class_counts) do
 		local weights = {}
@@ -316,7 +318,7 @@ for hero in paths.iterdirs("data") do
 			local input_len = move_data[1][1]:size(2)
 			local output_len = 13 + #ability_data.activeAbilities[hero][2] + #ability_data.activeItems[hero][2]
 
-			local move = CreateContainer(input_len, output_len, math.floor((input_len + output_len) / 2), 3)
+			local move = CreateContainer(input_len, output_len, math.floor((input_len + output_len) / 2))
 
 			print("\nMoving:")
 			Train(move, move_data, Loss(move_label_weights, move_class_weights), {3, 8, #ability_data.activeAbilities[hero][2] + 1, #ability_data.activeItems[hero][2] + 1})
@@ -339,7 +341,7 @@ for hero in paths.iterdirs("data") do
 			local input_len = move_data[1][1]:size(2)
 			local output_len = 13 + #ability_data.activeAbilities[hero][3] + #ability_data.activeItems[hero][3]
 
-			local move = CreateContainer(input_len, output_len, math.floor((input_len + output_len) / 2), 3)
+			local move = CreateContainer(input_len, output_len, math.floor((input_len + output_len) / 2))
 
 			print("\nMoving:")
 			Train(move, move_data, Loss(move_label_weights, move_class_weights), {3, 8, #ability_data.activeAbilities[hero][3] + 1, #ability_data.activeItems[hero][3] + 1})
